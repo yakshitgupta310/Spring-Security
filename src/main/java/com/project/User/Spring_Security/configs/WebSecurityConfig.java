f144a8cb1bb64b6358c22ade8fca3b41c266f868
yakshitgupta310 <65021783+yakshitgupta310@users.noreply.github.com>
2025-01-14 11:27:04 +0530

    Disabled authentication for register Mapping

--- src/main/java/com/project/User/Spring_Security/configs/WebSecurityConfig.java
+++ src/main/java/com/project/User/Spring_Security/configs/WebSecurityConfig.java
@@ -5,8 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,9 +28,21 @@ public class WebSecurityConfig {
         httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(
-                        request -> request.anyRequest().authenticated())
+                        request -> request.requestMatchers("register").permitAll()
+                                .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1 = User.withUsername("user1")
+                .password("{noop}user1410").build();
+
+        UserDetails user2 = User.withUsername("user2")
+                .password("{noop}user2410").build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 
 
