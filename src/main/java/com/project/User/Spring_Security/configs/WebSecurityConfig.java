7982c43dee9ea8a56d05cb99839252c6cde13463
yakshitgupta310 <65021783+yakshitgupta310@users.noreply.github.com>
2025-01-14 10:54:20 +0530

    Created basic SecurityFilterChain with csrf disabled

--- src/main/java/com/project/User/Spring_Security/configs/WebSecurityConfig.java
+++ src/main/java/com/project/User/Spring_Security/configs/WebSecurityConfig.java
@@ -2,18 +2,32 @@ package com.project.User.Spring_Security.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
-//@EnableWebSecurity
+@Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
    /* @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder(11);
     }*/
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(
+                        request -> request.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
 
 
 }
